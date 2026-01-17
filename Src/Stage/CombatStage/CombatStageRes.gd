97d30257f794d9db63c8791d7f2a983d0049836c
RandallSungcang <randallsungcang3@gmail.com>
2026-01-17 17:09:33 -0600

    Setting up CombatScene and Nodes, adding/connecting scripts next

--- Src/Stage/CombatStage/CombatStageRes.gd
+++ Src/Stage/CombatStage/CombatStageRes.gd
@@ -6,3 +6,4 @@ class_name CombatStageRes
 @export var enemy_slots: int
 
 @export var list_of_enemies: Array[EnemyRes] = []
+@export var combat_bg: Texture2D
--- Src/Stage/StageRes.gd
+++ Src/Stage/StageRes.gd
@@ -9,3 +9,4 @@ class_name StageRes
 @export var next_stage: StageRes
 
 @export var stage_prefab: PackedScene
+@export var background: Texture2D
