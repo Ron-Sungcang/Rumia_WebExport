9d9aa28a40fcad5e89c527cfead21806159e7a37
RandallSungcang <randallsungcang3@gmail.com>
2026-02-16 22:27:37 -0600

    Units are now interactable

--- Src/Units/PartyUnits/PartyUnit.gd
+++ Src/Units/PartyUnits/PartyUnit.gd
@@ -2,17 +2,17 @@ extends Unit
 
 class_name PartyUnit
 
+@export var texture_button: TextureButton
 #@export var area_2d: Area2D
 
 func _ready() -> void:
-	#if area_2d == null:
-		#print("PartyUnit, unset collision box")
-		#return
-	#
-	#area_2d.mouse_entered.connect(_on_mouse_entered)
-	#area_2d.mouse_exited.connect(_on_mouse_exited)
-	#area_2d.input_event.connect(_on_area_input_event)
-	pass
+	if texture_button == null:
+		print("EnemyUnit, unset collision box")
+		return
+	
+	texture_button.mouse_entered.connect(_on_mouse_entered)
+	texture_button.mouse_exited.connect(_on_mouse_exited)
+	texture_button.pressed.connect(_on_pressed)
 
 
 func _process(delta: float) -> void:
@@ -26,22 +26,14 @@ func initialize(party_res: PartyRes) -> void:
 	set_sprite(party_res.unit_image)
 
 
-func _on_area_input_event(
-	viewport: Node,
-	event: InputEvent,
-	shape_idx: int
-) -> void:
-	if event is InputEventMouseButton \
-	and event.button_index == MOUSE_BUTTON_LEFT \
-	and event.pressed:
+func _on_pressed() -> void:
+	UnitManager.selected_enemy_unit = null
+	UnitManager.selected_party_unit = self
 		
-		UnitManager.selected_enemy_unit = null
-		UnitManager.selected_party_unit = self
+	print("CLICKED ", UnitManager.selected_party_unit.unit_name)
 		
-		print("CLICKED ", UnitManager.selected_party_unit.unit_name)
-		
-		if UnitManager.selected_enemy_unit != null:
-			print("ERROR: Enemy Unit still selected")
+	if UnitManager.selected_enemy_unit != null:
+		print("ERROR: Enemy Unit still selected")
 
 
 func _on_mouse_entered() -> void:
